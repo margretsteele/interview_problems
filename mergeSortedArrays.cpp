24c72a4b63406c0b5858b951cda5ee6d477da058
Margret Steele <margret.steele@gmail.com>
2026-02-27 21:34:14 -0800

    Fix mergeSortedShit to handle remaining elements and use O(n+m) merge
    Previously elements from b were lost when a was exhausted. Replaced
    in-place insert approach with standard two-pointer merge into a new vector.

--- mergeSortedArrays.cpp
+++ mergeSortedArrays.cpp
@@ -1,30 +1,34 @@
 // Example program
 #include <iostream>
-#include <string>
 #include <vector>
 
-void mergeSortedShit(std::vector<int>& a, std::vector<int>& b) {
-    std::vector<int>::iterator ait = a.begin();
-    std::vector<int>::iterator bit = b.begin();
-    while(bit != b.end() && ait != a.end()) {
-        if(*bit >= *ait) {
-            ait++;
-        }
-        else {
-            ait = a.insert(ait, *bit);
-            bit++;
+std::vector<int> mergeSortedShit(const std::vector<int>& a, const std::vector<int>& b) {
+    std::vector<int> result;
+    result.reserve(a.size() + b.size());
+    auto ait = a.begin();
+    auto bit = b.begin();
+
+    while (ait != a.end() && bit != b.end()) {
+        if (*ait <= *bit) {
+            result.push_back(*ait++);
+        } else {
+            result.push_back(*bit++);
         }
     }
+    while (ait != a.end()) result.push_back(*ait++);
+    while (bit != b.end()) result.push_back(*bit++);
+
+    return result;
 }
 
 int main()
 {
     std::vector<int> a = { 1, 4, 7, 9, 12, 14 };
     std::vector<int> b = { 3, 6, 7, 7 };
-    mergeSortedShit(a, b);
-    
-    for (std::vector<int>::iterator it = a.begin() ; it != a.end(); ++it) {
-        std::cout << ' ' << *it;
+    auto merged = mergeSortedShit(a, b);
+
+    for (const auto& val : merged) {
+        std::cout << ' ' << val;
     }
-    
+    std::cout << '\n';
 }	
